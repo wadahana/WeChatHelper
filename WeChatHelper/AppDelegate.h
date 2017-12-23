984b63debea7e03f4adda32a017d596784dabb2a
wadahana <wadahana@gmail.com>
2017-12-23 10:53:51 +0800

    create project

---
+++ WeChatHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChatHelper
+//
+//  Created by wadahana on 23/12/2017.
+//  Copyright © 2017 wadahana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChatPlugin/WeChatPlugin.h
@@ -0,0 +1,19 @@
+//
+//  WeChatPlugin.h
+//  WeChatPlugin
+//
+//  Created by wadahana on 23/12/2017.
+//  Copyright © 2017 wadahana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeChatPlugin.
+FOUNDATION_EXPORT double WeChatPluginVersionNumber;
+
+//! Project version string for WeChatPlugin.
+FOUNDATION_EXPORT const unsigned char WeChatPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeChatPlugin/PublicHeader.h>
+
+
