2737bec8842509a09cc73554479f0b3c94ceddab
wadahana <wadahana@gmail.com>
2018-01-10 13:29:22 +0800

    add wechat class-dump header files

---
+++ WeChatPlugin/WeChatHeader/WeChatHeader.h
@@ -0,0 +1,19 @@
+//
+//  WeChatHeader.h
+//  WeChatHelper
+//
+//  Created by wadahana on 08/01/2018.
+//  Copyright © 2018 wadahana. All rights reserved.
+//
+
+#ifndef WeChatHeader_h
+#define WeChatHeader_h
+
+#import <MMUICommonUtil.h>
+#import <MMUIViewControllerDelegate.h>
+#import <CContact.h>
+#import <ContactSelectViewDelegate.h>
+#import <MMServiceCenter.h>
+#import <ContactSelectView.h>
+
+#endif /* WeChatHeader_h */
